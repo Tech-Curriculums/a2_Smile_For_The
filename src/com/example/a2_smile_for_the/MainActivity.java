8793deb61ac0db2fec1aaaa3c29ac6caa4512457
gskielian <gregory.kielian@gmail.com>
2014-03-21 00:36:08 -0700

    Can now take image, return and show on initial screen.

--- src/com/example/a2_smile_for_the/MainActivity.java
+++ src/com/example/a2_smile_for_the/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.a2_smile_for_the;
 
-import android.os.Bundle;
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
 
 public class MainActivity extends Activity {
+
+	ImageView image;
+	Button button;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		addListenerOnButton();
+ 
+	}
+ 
+	public void addListenerOnButton() {
+ 
+ 
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+                dispatchTakePictureIntent();
+			}
+ 
+		});
+ 
 	}
 
 	@Override
@@ -19,4 +53,21 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            image = (ImageView) findViewById(R.id.imageView1);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
+        }
+    }
+    
 }
